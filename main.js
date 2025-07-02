01241bf7f70409b5f84741f387e13c7ff3036186
OOM <157845784+asp3ct-9@users.noreply.github.com>
2025-07-03 01:41:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+ new Typed("#typed", {
+    strings: ["Web Development", "AI Solutions", "Cloud Systems", "Cybersecurity"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+  });
+
+  AOS.init({
+    duration: 1000,
+    once: true,
+  });
+
+ AOS.init();
